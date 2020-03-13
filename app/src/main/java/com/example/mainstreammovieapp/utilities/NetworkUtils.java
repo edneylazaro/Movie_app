6e4251c7563eed2ae27f36ca7fca47b672ddd525
Edney Lázaro <edney.lazaro@itsector.pt>
2020-03-13 17:12:48 +0000

    13/3/2020

---
+++ app/src/main/java/com/example/mainstreammovieapp/utilities/NetworkUtils.java
@@ -0,0 +1,7 @@
+package com.example.mainstreammovieapp.utilities;
+
+public final class NetworkUtils {
+
+    private static final String OMBD_API_MOVIE_DATABASE =
+            "http://www.omdbapi.com/?apikey=[3bf42759]&";
+}
---
+++ app/src/main/java/com/example/mainstreammovieapp/utilities/OpenMovieJsonUtils.java
@@ -0,0 +1,4 @@
+package com.example.mainstreammovieapp.utilities;
+
+public final class OpenMovieJsonUtils {
+}
